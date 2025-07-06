488ce405a2fdef18169778b8b70cccdcd2986bf6
mansol <qkr107781@gmail.com>
2025-07-06 18:42:49 +0900

    [chore] - 프로젝트 최초 세팅

---
+++ src/test/java/io/hhplus/tdd/TddApplicationTests.java
@@ -0,0 +1,19 @@
+package io.hhplus.tdd;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+class TddApplicationTests {
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
